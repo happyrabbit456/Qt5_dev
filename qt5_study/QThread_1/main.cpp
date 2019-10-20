f71e9d07345cf628203c80a9afc21fac653a17ef
happyrabbit456 <530633668@qq.com>
2019-10-20 13:07:10 +0800

    1.QObject::moveToThread()

---
+++ qt5_study/QThread_1/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include "controller.h"
+#include<QDebug>
+#include<QThread>
+
+int main(int argc, char *argv[])
+{
+    qDebug()<<"I am main Thread, my ID:"<<QThread::currentThreadId()<<"\n";
+
+    QCoreApplication a(argc, argv);
+
+    Controller c;
+
+    return a.exec();
+}
